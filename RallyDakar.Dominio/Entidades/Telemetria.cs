aa53356b40155b65b0bb846fde715d63453ef712
Leonardo Souza <leosouza@haacademy.co>
2020-05-08 08:14:13 -0700

    Organizando classe telemetria

--- RallyDakar.Dominio/Entidades/Telemetria.cs
+++ RallyDakar.Dominio/Entidades/Telemetria.cs
@@ -7,25 +7,19 @@ namespace RallyDakar.Dominio.Entidades
         public int Id { get; set; }
         public int TemporadaId { get; set; }
         public int PilotoId { get; set; }
-
-        public DateTime  Data { get; set; }
+        public DateTime Data { get; set; }
         public TimeSpan Hora { get; set; }
-
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
-
         public decimal PercentualCombustivel { get; set; }
         public double Velocidade { get; set; }
-
         public double RPM { get; set; }
-
         public int TemperaturaExterna { get; set; }
         public int TemperaturaMotor { get; set; }
-
         public double AltitudeNivelMar { get; set; }
 
         public bool PedalAcelerador { get; set; }
         public bool PedalFreio { get; set; }
-        
+
     }
 }
