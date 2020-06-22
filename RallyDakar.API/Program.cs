a3d149606f0ece701bb4fef98d580ae7096ba2f8
Leonardo Souza <leosouza@haacademy.co>
2020-06-22 07:14:18 -0700

    Carga Inicial Dados

--- RallyDakar.API/Program.cs
+++ RallyDakar.API/Program.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
+using RallyDakar.Dominio.DbContexto;
 
 namespace RallyDakar.API
 {
@@ -21,8 +23,18 @@ namespace RallyDakar.API
             logger.Info("Iniciando o applicativo");
             try
             {
-                CreateHostBuilder(args).Build().Run();
-            }catch(Exception ex)
+                //CreateHostBuilder(args).Build().Run();
+                var host = CreateHostBuilder(args).Build();
+                
+                using(var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    BaseDados.CargaInicial(services);
+                }
+
+                host.Run();
+            }
+            catch(Exception ex)
             {
                 logger.Error(ex, "Aplicação paraou de rodar");
             }
---
+++ RallyDakar.Dominio/DbContexto/BaseDados.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RallyDakar.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RallyDakar.Dominio.DbContexto
+{
+    public class BaseDados
+    {
+        public static void CargaInicial(IServiceProvider serviceProvider)
+        {
+            using(var context = new RallyDbContexto(serviceProvider.GetRequiredService<DbContextOptions<RallyDbContexto>>()))
+            {
+                var temporada = new Temporada();
+                temporada.Id = 1;
+                temporada.Nome = "Temporada2020";
+                //temporada.DataInicio = DateTime.Now.AddDays(20); Inicia daqui a 20 dias
+                temporada.DataInicio = DateTime.Now;
+
+                var equipe = new Equipe();
+                equipe.Id = 1;
+                equipe.Nome = "Azul";
+                equipe.CodigoIdentificador = "AZL";
+
+                var pilotoPedro = new Piloto();
+                pilotoPedro.Id = 1;
+                pilotoPedro.Nome = "Pedro";
+
+
+                var pilotoCarlos = new Piloto();
+                pilotoCarlos.Id = 2;
+                pilotoCarlos.Nome = "Carlos";
+
+                equipe.AdicionarPiloto(pilotoPedro);
+                equipe.AdicionarPiloto(pilotoCarlos);
+
+                temporada.AdicionarEquipe(equipe);
+
+                context.Temporadas.Add(temporada);
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
